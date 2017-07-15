18a4dab692d11b2b7b306bb718f749e4c162fa90
xujunyu <1098672878@qq.com>
2017-07-15 11:00:09 +0800

    添加静态资源映射

--- spring-mvc/src/main/java/mvc/MyMvcConfig.java
+++ spring-mvc/src/main/java/mvc/MyMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -13,15 +15,27 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan("mvc")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
-    public InternalResourceViewResolver viewResolver(){
+    public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/classes/views/");
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    /**
+     * 设置对静态资源的映射
+     * addResourceHandler   对外暴露的接口
+     * addResourceLocations 文件放置的目录
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/asserts/");
     }
 
 }
